36e7834192d817699587d125ce738ba137d4a96e
Josh Shavers <shaversj@gmail.com>
2023-06-23 14:43:23 -0500

    Updated min viewport height

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import OrderSummary from "./OrderSummary.jsx";
 
 function App() {
 
-  return (
-    <>
-      <div className={"container min-h-[100vh] bg-mobile sm:bg-no-repeat lg:bg-desktop bg-pale-blue min-w-full grid place-items-center"}>
-          <OrderSummary />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div
+                className={"container min-h-[100vh] bg-mobile sm:bg-no-repeat lg:bg-desktop bg-pale-blue min-w-full grid place-items-center"}>
+                <OrderSummary/>
+            </div>
+        </>
+    )
 }
 
 export default App
