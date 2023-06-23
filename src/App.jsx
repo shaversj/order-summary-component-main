3944439db0e0e590d8d86386e66f00bd4e8c1943
Josh Shavers <shaversj@gmail.com>
2023-06-23 14:41:59 -0500

    Updated min viewport height

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className={"container bg-mobile sm:bg-no-repeat lg:bg-desktop bg-pale-blue min-h-screen min-w-full grid place-items-center"}>
+      <div className={"container min-h-[100vh] bg-mobile sm:bg-no-repeat lg:bg-desktop bg-pale-blue min-w-full grid place-items-center"}>
           <OrderSummary />
       </div>
     </>
